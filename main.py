beb1de3c55971ee1828903132d411fa03873ccc4
Kerem Alagöz <150536980+keremalagoz@users.noreply.github.com>
2025-06-20 00:56:49 +0300

    Delete main.py

--- main.py
+++
@@ -1,4 +0,0 @@
-from content_based_recommender import ContentBasedRecommender
-
-recommender = ContentBasedRecommender('movies.csv')
-recommendations = recommender.get_recommendations([101, 102, 108], 5)
